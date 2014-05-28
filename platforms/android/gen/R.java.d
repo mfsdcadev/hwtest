f32c0f90767653ffccfdc41974fb4fc507190b57
mfsdcadev <mfsdca.dev@gmail.com>
2014-05-28 08:14:23 -0700

    cordova build

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+C:\mf\cl\auphonic\hwtest\platforms\android\gen\io\cordova\hellocordova\R.java \
+ : C:\mf\cl\auphonic\hwtest\platforms\android\res\drawable\icon.png \
+C:\mf\cl\auphonic\hwtest\platforms\android\res\drawable-hdpi\icon.png \
+C:\mf\cl\auphonic\hwtest\platforms\android\res\drawable-hdpi\ic_launcher.png \
+C:\mf\cl\auphonic\hwtest\platforms\android\res\drawable-ldpi\icon.png \
+C:\mf\cl\auphonic\hwtest\platforms\android\res\drawable-ldpi\ic_launcher.png \
+C:\mf\cl\auphonic\hwtest\platforms\android\res\drawable-mdpi\icon.png \
+C:\mf\cl\auphonic\hwtest\platforms\android\res\drawable-mdpi\ic_launcher.png \
+C:\mf\cl\auphonic\hwtest\platforms\android\res\drawable-xhdpi\icon.png \
+C:\mf\cl\auphonic\hwtest\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+C:\mf\cl\auphonic\hwtest\platforms\android\res\layout\main.xml \
+C:\mf\cl\auphonic\hwtest\platforms\android\res\values\strings.xml \
+C:\mf\cl\auphonic\hwtest\platforms\android\res\xml\config.xml \
+C:\mf\cl\auphonic\hwtest\platforms\android\bin\AndroidManifest.xml \
